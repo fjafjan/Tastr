ff30c0be3024aa0e691e82b6152564d29da5b508
Staffan Ankardal <staffan.ankardal@gmail.com>
2024-08-10 09:41:25 +0200

    Deploying to github pages

--- tastr-project/vite.config.js
+++ tastr-project/vite.config.js
@@ -9,21 +9,40 @@ import { createHtmlPlugin } from 'vite-plugin-html';
 export default defineConfig(({ mode }) => {
   const isDevelopment = mode === 'development';
 
+  const extensions = [
+    ".web.tsx",
+    ".tsx",
+    ".web.ts",
+    ".ts",
+    ".web.jsx",
+    ".jsx",
+    ".web.js",
+    ".js",
+    ".css",
+    ".json",
+    ".mjs",
+  ];
+
+  const development = process.env.NODE_ENV === "development";
+
   return {
     root: './', // Ensure the root is set correctly for Vite to find the entry point
     plugins: [
-      react({
-        // Use React plugin in all *.jsx and *.tsx files
-        include: '**/*.{jsx,tsx}',
-      }),      createHtmlPlugin({
-        template: 'index.html',
-      }),
+      react(),
     ],
+    define: {
+      // https://github.com/bevacqua/dragula/issues/602#issuecomment-1296313369
+      global: "window",
+      __DEV__: JSON.stringify(development),
+      // https://tamagui.dev/docs/intro/installation
+      DEV: JSON.stringify(development),
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    },
     resolve: {
       alias: {
-        'react-native': path.resolve(__dirname, 'node_modules/react-native-web'),
+        'react-native': 'react-native-web',
       },
-      extensions: ['.web.js', '.js', '.jsx', '.json'],
+      extensions: extensions,
     },
     build: {
       outDir: 'dist',
@@ -48,8 +67,15 @@ export default defineConfig(({ mode }) => {
         // },
       },
     },
-    define: {
-      __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
+    optimizeDeps: {
+      esbuildOptions: {
+        resolveExtensions: extensions,
+        // https://github.com/vitejs/vite-plugin-react/issues/192#issuecomment-1627384670
+        jsx: "automatic",
+        // need either this or the plugin below
+        loader: { ".js": "jsx" },
+
+      },
     },
   };
 });
