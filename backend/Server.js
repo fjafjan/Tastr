6d2fbdc96c75d86e4fa6c6755131ebe183dd0381
Staffan Ankardal <staffan.ankardal@gmail.com>
2024-08-22 22:02:35 +0200

    Fix typo

--- backend/Server.js
+++ backend/Server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const https = require('https')
+const http = require('http')
 const fs = require('fs')
 const path = require('path')
 const cors = require('cors')
