5c2c62076cc25f25713c5ce773986194745431b2
Staffan Ankardal <staffan.ankardal@gmail.com>
2024-10-06 21:08:05 +0200

    Adding tests

--- test/food_category_controller.test.ts
+++ test/food_category_controller.test.ts
@@ -1,8 +1,44 @@
-import { addCategory } from "../backend/controllers/food_category_controller"; // Adjust the import as needed
+import { addCategory, getAliases, getMmr, getNames } from "../backend/controllers/food_category_controller"; // Adjust the import as needed
 import * as DatabaseUtility from "../backend/database_utility";
 import { Request, Response } from "express";
-
+import { FoodCategoryData, IFoodCategory } from "../backend/models";
+import { ParamsDictionary } from 'express-serve-static-core'
 jest.mock("../backend/database_utility");
+
+const mockFoodObject =
+{
+  id: "test-food-id",
+  name: "test-name",
+  alias: "test-alias",
+  voteCount: 5,
+  MMR: 1000,
+};
+
+const mockFoodCategory = {
+  categoryId: "test-category",
+  foodObjects:
+  [
+    mockFoodObject,
+  ],
+} as IFoodCategory
+
+function foodCategoryParams(): ParamsDictionary {
+  return { foodCategoryId: "test-category"}
+}
+
+const validCategoryRequest = {
+  body: {
+    categoryId: "test-category",
+  },
+  params: foodCategoryParams()
+} as Request
+
+function GetMockResponse(): Response {
+  return {
+    json: jest.fn(),
+    sendStatus: jest.fn(),
+  } as any as Response;
+}
 
 describe("FoodCategoryController", () => {
   it("should add a new category", async () => {
@@ -16,11 +52,7 @@ describe("FoodCategoryController", () => {
       },
     } as Request;
 
-    const mockResponse = {
-      json: jest.fn(),
-      sendStatus: jest.fn(),
-    } as any as Response;
-
+    const mockResponse = GetMockResponse();
     // Mock the CreateCategory function to return true
     (DatabaseUtility.CreateCategory as jest.Mock).mockResolvedValue(true);
 
@@ -29,4 +61,42 @@ describe("FoodCategoryController", () => {
   });
 
   // Add more tests as needed
-});
+
+  it("Should retrieve names from category", async () => {
+    const mockResponse = GetMockResponse();
+
+    // Mock response value from data.
+    // (FoodCategoryData.findOne as jest.Mock).mockResolvedValue(mockFoodCategory)
+    // jest.spyOn(FoodCategoryData, 'findOne').mockReturnValue(mockFoodCategory)
+    FoodCategoryData.findOne = jest.fn().mockResolvedValue([mockFoodCategory])
+
+    const names = await getNames( validCategoryRequest, mockResponse);
+
+    expect(mockResponse.sendStatus).toHaveBeenCalledWith(200)
+    expect(names).toBe([mockFoodObject.name])
+   })
+
+   it("Should retrieve aliases from category", async () => {
+    const mockResponse = GetMockResponse();
+
+    // Mock response value from data.
+    (FoodCategoryData.findOne as jest.Mock).mockResolvedValue(mockFoodCategory)
+
+    const names = await getAliases(validCategoryRequest, mockResponse);
+
+    expect(mockResponse.sendStatus).toHaveBeenCalledWith(200)
+    expect(names).toBe([mockFoodObject.alias])
+   })
+
+   it("Should retrieve MMR from category", async () => {
+    const mockResponse = GetMockResponse();
+
+    // Mock response value from data.
+    (FoodCategoryData.findOne as jest.Mock).mockResolvedValue(mockFoodCategory)
+
+    const names = await getMmr(validCategoryRequest, mockResponse);
+
+    expect(mockResponse.sendStatus).toHaveBeenCalledWith(200)
+    expect(names).toBe([mockFoodObject.MMR])
+   })
+  });
