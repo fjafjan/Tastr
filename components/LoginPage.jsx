d4decc5fe14d461e8e4dceb5c5e6a15bae7eea2c
Staffan Ankardal <staffan.ankardal@gmail.com>
2024-11-16 21:57:41 +0100

    Add missing import

--- components/LoginPage.jsx
+++ components/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SERVER_URL } from "../constants/Constants";
 import { useNavigate, useParams } from "react-router-dom";
 import {
